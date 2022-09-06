1d34ceb0c9af0269ed69c6746a0c769a38dc38f2
Marcio Bautista <marcio.bautista@rootstrap.com>
2022-09-06 16:40:58 -0300

    solve ex 1 extra 4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,24 +4,23 @@
 import * as React from 'react'
 
 function countReducer(state, action) {
-  if (typeof action === 'function') {
-    return {
-      ...state,
-      ...action(state),
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + action.step }
+    default:
+      return { ...state };
   }
-  return { ...state, ...action };
 }
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, setState] = React.useReducer(countReducer, { count: initialCount })
+  const [state, dispatch] = React.useReducer(countReducer, { count: initialCount })
   const { count } = state;
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setState(currentState => ({ count: currentState.count + step }))
+  const increment = () => dispatch({ type: 'INCREMENT', step })
   return <button onClick={increment}>{count}</button>
 }
 
